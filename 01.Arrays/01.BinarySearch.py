04d667a0999ec4eb2c0a310a409c5e6838673d61
NihalSayyad <nihalsayyad912@gmail.com>
2021-11-02 20:22:11 +0530

    Binary Search - recursive call

--- 01.Arrays/01.BinarySearch.py
+++ 01.Arrays/01.BinarySearch.py
@@ -15,11 +15,26 @@ def binary_search(myList, target):
         if mid_number < target:
             left_index = mid_index + 1
         else:
-            right_index = mid_index
+            right_index = mid_index - 1
 
     return -1
+
+def binary_search_recursive(myList, target, left_index, right_index):
+    if left_index > right_index:
+        return -1
+
+    mid_index = (left_index + right_index) // 2
+    mid_number = myList[mid_index]
+
+    if mid_number == target:
+        return mid_index
+
+    if mid_number < target:
+        return binary_search_recursive(myList, target, mid_index+1, right_index)
+    else:
+        return binary_search_recursive(myList, target, left_index, mid_index-1)
 
 if __name__ == '__main__':
     numbers = [2, 4, 7, 8, 10, 23, 27, 32, 33, 39, 42]
     target = 32
-    print(f"Number found at index {binary_search(numbers, target)}")
+    print(f"Number found at index {binary_search_recursive(numbers, target, 0, len(numbers) - 1)}")
