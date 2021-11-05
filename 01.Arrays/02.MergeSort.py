924139e796672665612f0dff7d8b7f7ac2645588
NihalSayyad <nihalsayyad912@gmail.com>
2021-11-05 20:13:17 +0530

    MergeSort - merge two sorted arrays

--- 01.Arrays/02.MergeSort.py
+++ 01.Arrays/02.MergeSort.py
@@ -1,1 +1,28 @@
 #Merge Sort
+
+def merge_two_sorted_arrays(a, b):
+    n = len(a)
+    m = len(b)
+    i = j = 0
+    c =[]
+    while i<n and j< m:
+        if a[i] < b[j]:
+            c.append(a[i])
+            i += 1
+        else:
+            c.append(b[j])
+            j += 1
+
+    while i < n:
+        c.append(a[i])
+        i += 1
+    while j < m:
+        c.append(b[j])
+        j += 1
+    return c
+
+if __name__ == '__main__':
+    a = [5,8,12,56]
+    b = [7,9,45,51]
+
+    print(merge_two_sorted_arrays(a,b))
