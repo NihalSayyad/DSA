d5ddf56e288d20d33dd42b4cd170609d59be1753
NihalSayyad <nihalsayyad912@gmail.com>
2021-11-06 19:01:43 +0530

    QuickSort

---
+++ 01.Arrays/03.QuikSort.py
@@ -0,0 +1,33 @@
+#Partition
+
+def partition(low, high, arr):
+    pivot = arr[low]
+    i = low
+    j = high
+    while i < j:
+        while True:
+            i += 1
+            if arr[i] > pivot:
+                break
+        while True:
+            j -= 1
+            if arr[j] <= pivot:
+                break
+        if i < j :
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[low], arr[j] = arr[j], arr[low]
+    return j
+
+#Quick sort
+def quick_sort(low, high, arr):
+    if low < high:
+        j = partition(low, high, arr)
+        quick_sort(low, j, arr)
+        quick_sort(j+1, high, arr)
+
+
+if __name__ == '__main__':
+    arr = [5,3,4,1,6,7,9,10]
+    quick_sort(0, len(arr)-1, arr)
+    print(arr)
