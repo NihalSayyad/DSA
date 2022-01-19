ce2584af7e5bcb5cdf5b3a4ec92310a9759e0c05
NihalSayyad <nihalsayyad912@gmail.com>
2022-01-19 22:08:57 +0530

    Basics - stack

--- 03.Stack/01.StringReversal.py
+++ 03.Stack/01.StringReversal.py
@@ -1,1 +1,10 @@
-import Implementation
+from Implementation import Stack
+
+line = "This is a line"
+stack = Stack()
+for i in range(len(line)):
+    stack.push(line[i])
+
+for i in range(len(line)):
+    print(stack.pop(), end='')
+
