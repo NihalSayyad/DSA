f55aeb6f8ed67b471b41b9eef7e3f9f10bdbb283
NihalSayyad <nihalsayyad912@gmail.com>
2022-01-19 21:56:43 +0530

    Basics - stack

---
+++ 03.Stack/00.Implementation.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+class Stack:
+    def __init__(self):
+        self.container = deque()
+
+    def push(self, val):
+        self.container.append(val)
+
+    def pop(self):
+        return self.container.pop()
+
+    def peek(self):
+        return self.container[-1]
+
+    def is_empty(self):
+        return len(self.container) == 0
