d2bf84f7663721d97a7ed8f8911f888ba3578da9
NihalSayyad <nihalsayyad912@gmail.com>
2022-01-20 20:00:52 +0530

    Basics - queue

--- 04.Queue/Implementation.py
+++ 04.Queue/Implementation.py
@@ -13,3 +13,27 @@ class Queue:
 
     def is_empty(self):
         return len(self.buffer) == 0
+
+if __name__ == '__main__':
+    pq = Queue()
+    pq.enqueue({
+        'Company' : 'Wal Mart',
+        'timestamp' : '20 Jan, 11.15 AM',
+        'price' : 131.10
+    })
+
+    pq.enqueue({
+        'Company': 'Wal Mart',
+        'timestamp': '20 Jan, 11.16 AM',
+        'price': 132
+    })
+
+    pq.enqueue({
+        'Company': 'Wal Mart',
+        'timestamp': '20 Jan, 11.17 AM',
+        'price': 135
+    })
+
+    while not pq.is_empty():
+        data = pq.dequeue()
+        print(data)
