c1ec27d1f0c66fbb7ea4e4d844db3dba8bfe8afd
NihalSayyad <nihalsayyad912@gmail.com>
2022-01-22 13:29:33 +0530

    Basics - multi proc - pool

--- 00.Basics/16.multiPorc_Pool.py
+++ 00.Basics/16.multiPorc_Pool.py
@@ -1,11 +1,24 @@
 import multiprocessing
+import time
 
 def f(n):
-    return n*n
+    sum = 0
+    for x in range(1000):
+        sum += x*x
+    return sum
 
 if __name__ == '__main__':
-    array = [1,2,3,4,5,6]
+    t1 = time.time()
     p = multiprocessing.Pool()
-    result = p.map(f, array)
+    result = p.map(f, range(100000))
+    p.close()
+    p.join()
 
-    print(result)
+    print("Pool took : ", time.time() - t1)
+
+    t2 = time.time()
+    result = []
+    for x in range(100000):
+        result.append(f(x))
+
+    print("Serial processing took : ", time.time() - t2)
