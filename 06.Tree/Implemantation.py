8db0e6f453f73035e60fa40186b74f9ec2b08233
NihalSayyad <nihalsayyad912@gmail.com>
2022-01-28 21:11:11 +0530

    Basics - General Tree

---
+++ 06.Tree/Implemantation.py
@@ -0,0 +1,53 @@
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+        self.parent = None
+
+    def add_child(self, child):
+        child.parent = self
+        self.children.append(child)
+
+    def get_level(self):
+        level = 0
+        p = self.parent
+        while p:
+            level += 1
+            p = p.parent
+        return level
+
+    def print_tree(self):
+        spaces = ' ' * self.get_level() * 3
+        prefix = spaces + "|--"
+        print(prefix + self.data)
+        if self.children:
+            for child in self.children:
+                child.print_tree()
+
+def build_product_tree():
+    root = TreeNode("Electronics")
+
+    laptop = TreeNode("Laptop")
+    laptop.add_child(TreeNode("Mac"))
+    laptop.add_child(TreeNode("Surface"))
+    laptop.add_child(TreeNode("Thinkpad"))
+
+    cellphone = TreeNode("Cell Phone")
+    cellphone.add_child(TreeNode("iPhone"))
+    cellphone.add_child(TreeNode("Pixel"))
+    cellphone.add_child(TreeNode("Vivo"))
+
+    tv = TreeNode("TV")
+    tv.add_child(TreeNode("LG"))
+    tv.add_child(TreeNode("Samsung"))
+
+    root.add_child(laptop)
+    root.add_child(cellphone)
+    root.add_child(tv)
+
+    return root
+
+
+if __name__ == '__main__':
+    root = build_product_tree()
+    root.print_tree()
