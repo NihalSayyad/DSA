69388d68e4ba6e6506be27d4114fbbec129a7193
NihalSayyad <nihalsayyad912@gmail.com>
2022-01-29 18:57:29 +0530

    Basics - BST

--- 06.Tree/BinarySearchTree.py
+++ 06.Tree/BinarySearchTree.py
@@ -34,6 +34,22 @@ class BinarySearchTreeNode:
 
         return elements
 
+    def search(self, val):
+        if self.data == val:
+            return True
+
+        if val < self.data:
+            if self.left:
+                return self.left.search(val)
+            else:
+                return False
+
+        if val > self.data:
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
+
 
 def build_tree(elements):
     root = BinarySearchTreeNode(elements[0])
@@ -46,3 +62,5 @@ if __name__ == '__main__':
     numbers = [17, 4, 1, 20, 9, 23, 18, 34]
     root = build_tree(numbers)
     print(root.in_order_traversal())
+
+    print(root.search(20))
