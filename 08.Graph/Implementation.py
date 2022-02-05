c389408859ef32845decff53d58ad41c4cc187ad
NihalSayyad <nihalsayyad912@gmail.com>
2022-02-05 20:27:05 +0530

    Basics - Graph

--- 08.Graph/Implementation.py
+++ 08.Graph/Implementation.py
@@ -35,6 +35,25 @@ class Graph:
 
         return paths
 
+    def get_shortest_path(self, start, end, path=[]):
+        path = path + [start]
+        if start == end:
+            return path
+
+        if start not in self.graph_dict:
+            return None
+
+        shortest_path = None
+        for node in self.graph_dict[start]:
+            if node not in path:
+                sp = self.get_shortest_path(node, end, path)
+                if sp:
+                    if shortest_path is None or len(sp) < len(shortest_path):
+                        shortest_path = sp
+
+        return shortest_path
+
+
 if __name__ == '__main__':
     routes = [
         ("Mumbai", "Paris"),
@@ -48,4 +67,5 @@ if __name__ == '__main__':
     route_graph = Graph(routes)
     start = "Mumbai"
     end = "New York"
-    print(f"Paths between {start} and {end} : ",route_graph.get_paths(start, end) )
+    print(f"Paths between {start} and {end} : ",route_graph.get_shortest_path(start, end) )
+
